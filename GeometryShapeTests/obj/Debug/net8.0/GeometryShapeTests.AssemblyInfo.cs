5e1010c31e36a03ed024e12822df8985f9d037db
IlyaKirichenko <ikirichenko345@gmail.com>
2026-03-10 14:48:45 +0500

    TriangleTests fix

--- GeometryShapeTests/obj/Debug/net8.0/GeometryShapeTests.AssemblyInfo.cs
+++ GeometryShapeTests/obj/Debug/net8.0/GeometryShapeTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeometryShapeTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17f5c08338c85b453b3cd4b3dc1427d741499f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e6e67082885ffdab2101c1e75d5772ece1b78e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeometryShapeTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeometryShapeTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
